cbf8390766f4d3cc12bc02fc0d0a4c738cb04447
UtoPiiAx <122031843+UtoPiiAx@users.noreply.github.com>
2023-07-04 21:01:50 +0200

    Create GenericExceptionFailure.java

---
+++ Exercise06/Task02/GenericExceptionFailure.java
@@ -0,0 +1,7 @@
+public class GenericExceptionFailure {
+    /*
+    Zur Laufzeit ersetzt der Compiler T durch seine Beschränkung (oder durch Object)
+    und parametrisiert Typen durch Basisystem. Zur Laufzeit sind die Exceptions dann genau gleich,
+    weshalb die Fehlermeldung kommt.
+     */
+}
